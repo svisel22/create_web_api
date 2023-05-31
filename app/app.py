75cd5ff45f0c8a88a33ce58009af38d4135a683a
svisel22 <s_visel22@stud.hwr-berlin.de>
2023-05-31 18:32:12 +0200

    add files until docker compose

--- app/app.py
+++ app/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello', methods=['POST'])
 def hello():
-    return {'message': 'Hello World'}, 200
+    data = request.get_json()
+    name = data.get('name', '')
+    message = f'Hello {name}'
+    return {'message': message}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
